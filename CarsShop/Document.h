580db809f2a55a0a018a6c41d9b6901755448723
disco-disco <mr.razdva@gmail.ru>
2012-12-16 22:50:40 +0400

    Core Data project
    Небольшой проект для изучения основ Core Data

---
+++ CarsShop/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  CarsShop
+//
+//  Created by User on 13.12.12.
+//  Copyright (c) 2012 e. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+
+@end
---
+++ CarsShop/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CarsShop
+//
+//  Created by User on 13.12.12.
+//  Copyright (c) 2012 e. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
